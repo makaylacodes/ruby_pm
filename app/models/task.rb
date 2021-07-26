49ebe812ccb9bb2f5d32d70a16abf7b8e1899822
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-07-26 22:10:41 +0900

    I have made minor adjustments to the page design. Having an issue where the status is not appearing. Also when I click to update the project, I get an error. Once both of those issues are fixed, design is the last piece

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,24 @@
 class Task < ApplicationRecord
   belongs_to :project
 
-  validates :status, inclusion: { in: ['not-started', 'in-progress', 'complete']}
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'complete'] }
 
   STATUS_OPTIONS = [ 
-    [ 'Not Started', 'not-started'],
+    ['Not Started', 'not-started'],
     ['In Progress', 'in-progress' ],
     ['Complete', 'complete']
   ]
 
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+
+
+    end
+  end
 end

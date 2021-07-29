95fdd4546febebdba4930ea58556ef0aebefa190
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-07-29 09:18:43 +0900

    I have changed the background

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+config.serve_static_assets = false
+# Compress JavaScripts and CSS  
+config.stylesheets.compress = true
+# Generate digests for assets URLs  
+config.stylesheets.digest = true
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 

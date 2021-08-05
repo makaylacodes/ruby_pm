3450a95db1a3e7d77be0e82e3ca9672559491488
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-08-06 08:07:26 +0900

    going back to previous version

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module SimplePm
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.assets.initialize_on_precompile = false
-    config.secret_key = '--secret key--'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

fa66ac1a47fcccaa322af4f6da55855c8b94d753
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-08-06 08:09:30 +0900

    Revert "added config"
    This reverts commit 07484c9c2a59d0c2fdbac10fb7ff739c85349b13.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,12 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
- # Disable Rails's static asset server (Apache or nginx will already do this)  
- config.serve_static_assets = false
- # Compress JavaScripts and CSS  
- config.stylesheets.compress = true
- # Generate digests for assets URLs  
- config.stylesheets.digest = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
